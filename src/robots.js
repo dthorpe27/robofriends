301321f5d941b45ff9df0d2f093273ffdddba961
dthorpe27 <63649930+dthorpe27@users.noreply.github.com>
2020-05-15 15:32:33 +0100

    hope his works finally

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id:1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id:2,
+		name: 'Turnip Farmer',
+		username: 'Swede',
+		email: 'Turnip@april.biz'
+	},
+	{
+		id:3,
+		name: 'Erika Galloshaaaa',
+		username: 'Erika',
+		email: 'Erika@april.biz'
+	},
+	{
+		id:4,
+		name: 'Jemima Banana',
+		username: 'Jemima',
+		email: 'Jemima@april.biz'
+	},
+	{
+		id:5,
+		name: 'Wally Winka',
+		username: 'Wally',
+		email: 'Wally@april.biz'
+	},
+	{
+		id:6,
+		name: 'Jimbo Karakaka',
+		username: 'Jimbo',
+		email: 'Jimbo@april.biz'
+	},
+	{
+		id:7,
+		name: 'alababba Teleppo',
+		username: 'alababba',
+		email: 'Teleppo@april.biz'
+	},
+	{
+		id:8,
+		name: 'Sala Wala',
+		username: 'Salal',
+		email: 'Salwal@april.biz'
+	},
+	{
+		id:9,
+		name: 'Potato Tomato',
+		username: 'Chips',
+		email: 'chips@april.biz'
+	},
+	{
+		id:10,
+		name: 'Brian Bacon',
+		username: 'Porkie',
+		email: 'sausage@april.biz'
+	},
+]
